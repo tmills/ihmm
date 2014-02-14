b3492e5d6371dbd8284362dd3662b2c3880fc257
Tim Miller <tim.miller@gmail.com>
2014-02-14 07:59:04 -0500

    Added comments to script.

--- getSortedWords.m
+++ getSortedWords.m
@@ -1,11 +1,11 @@
 function [sorted,indices] = getSortedWords(S,Y,pos_idx)
 
 hid = S{end}.S;
-idx = 1:max(Y);
-pos_inds = find(hid==pos_idx);
-pos_words = Y(pos_inds);
-match_mat = (pos_words==idx);
-c_w_giv_p = sum(match_mat);
-c_w_giv_p = c_w_giv_p ./ sum(c_w_giv_p);
-[sorted,indices] = sort(c_w_giv_p);
+idx = 1:max(Y);   %% all word indices
+pos_inds = find(hid==pos_idx);   %% find all instances of given hmm state
+pos_words = Y(pos_inds);         %% get the word indices for those positions
+match_mat = (pos_words==idx);    %% build a matrix of counts 
+c_w_giv_p = sum(match_mat);      %% sum across word dimension
+c_w_giv_p = c_w_giv_p ./ sum(c_w_giv_p);   %% normalize
+[sorted,indices] = sort(c_w_giv_p);  %% sort by probability
 
